30e4b1cb377517209346fab72dc274c359961eda
-Samuca- <78044674+Samuel-Henrique-spec@users.noreply.github.com>
2023-11-19 15:08:19 -0300

    Update config.sh

--- i3/config.sh
+++ i3/config.sh
@@ -192,6 +192,8 @@ bar {
 exec_always xrandr -s 1920x1080 -r 120.00
 exec_always nitrogen --restore
 
+#i3 gaps
+gaps inner 10
 
 #i3locks
 bindsym $mod+Shift+p exec i3lock -i ~/Imagens/Wpp/gnu-linux-simple-wallpaper.png
